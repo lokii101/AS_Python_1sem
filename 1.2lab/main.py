56b8de89fec52439fc782ddd2c595dba5e6565db
lokii101 <vasy_1605@mail.ru>
2025-10-21 12:36:17 +0300

    Update main.py

--- 1.2lab/main.py
+++ 1.2lab/main.py
@@ -1,2 +1,9 @@
-if __name__ == "__main__":
-    pass
+A = int(input("A:"))
+B = int(input("B (B > A): "))
+H = int(input("H: "))
+if A >= B or H <= 0:
+    print(": A > B, а H > 0")
+else:
+    for number in range(A, B + 1, H):
+        square = number ** 2
+        print(f"2** {number} == {square}")
