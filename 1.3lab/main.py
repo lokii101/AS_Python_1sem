f08ed8a68751fecfadde14552f1f542bcee05381
lokii101 <vasy_1605@mail.ru>
2025-10-28 14:08:19 +0300

    Update main.py

--- 1.3lab/main.py
+++ 1.3lab/main.py
@@ -1,2 +1,10 @@
-if __name__ == "__main__":
-    pass
+#Для заданного натурального n подсчитать сумму:\begin{align} 1 - 2^3 + 3^3 - ... + (-1)^{n + 1}n^3. \end{align}
+def calculate_sum(n):
+    total = 0
+    for i in range(1, n + 1):
+        term = (-1) ** (i + 1) * (i ** 3)
+        total += term
+    return total
+n = int(input(" n: "))
+result = calculate_sum(n)
+print(f" n = {n}: {result}")
