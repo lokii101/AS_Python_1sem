c60dbc2ee22419215fa972b29c5d0c8ae46830e4
lokii101 <vasy_1605@mail.ru>
2025-10-28 14:30:09 +0300

    Update main.py

--- 1.5lab/main.py
+++ 1.5lab/main.py
@@ -1,2 +1,9 @@
-if __name__ == "__main__":
-    pass
+#Дан список. Выбрать из списка все числа больше заданного числа k и упорядочить их по убыванию.
+def filter_and_sort(numbers, k):
+    filtered = [num for num in numbers if num > k]
+    filtered.sort(reverse=True)
+    return filtered
+numbers = [10, 3, 7, 15, 2, 9, 12]
+k = 5
+result = filter_and_sort(numbers, k)
+print(result)
