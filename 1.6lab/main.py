e260e3ab546c37b072c93306fbd46fd13ec62091
lokii101 <vasy_1605@mail.ru>
2025-10-28 14:35:06 +0300

    Update main.py

--- 1.6lab/main.py
+++ 1.6lab/main.py
@@ -1,2 +1,11 @@
-if __name__ == "__main__":
-    pass
+def add_suffix(s):
+    if len(s) < 3:
+        return s
+    elif s.endswith('ing'):
+        return s + 'ly'
+    else:
+        return s + 'ing'
+
+print(add_suffix("go"))
+print(add_suffix("run"))
+print(add_suffix("playing"))
