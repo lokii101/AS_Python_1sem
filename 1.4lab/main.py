2805d6a200f021a8bb9531e6902e012d2507d95e
lokii101 <vasy_1605@mail.ru>
2025-11-06 12:30:26 +0300

    Update main.py

--- 1.4lab/main.py
+++ 1.4lab/main.py
@@ -1,2 +1,8 @@
-if __name__ == "__main__":
-    pass
+# Дан список. Вывести вначале его элементы с четными номерами (в порядке возрастания номеров), а затем — элементы с нечетными номерами (также в порядке возрастания номеров).
+lst = input().split()
+even_elements = [lst[i] for i in range(0, len(lst), 2)]
+odd_elements = [lst[i] for i in range(1, len(lst), 2)]
+result = even_elements + odd_elements
+print(' '.join(result))
+print(even_elements)
+print(odd_elements)
