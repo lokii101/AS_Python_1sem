e893f5b11422b07e7ed5cd0b7fb706fd4c5953fa
lokii101 <vasy_1605@mail.ru>
2025-11-11 14:14:37 +0300

    Update main.py

--- 2.2lab/main.py
+++ 2.2lab/main.py
@@ -1,2 +1,9 @@
-if __name__ == "__main__":
-    pass
+
+def RectPS(x1, y1, x2, y2):
+    width = abs(x2 - x1)
+    height = abs(y2 - y1)
+    P = 2 * (width + height)
+    S = width * height
+    return P, S
+P, S = RectPS(1, 1, 4, 5)
+print(P,S)
