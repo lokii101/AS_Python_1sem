46040de2c18bdca09f6a3abe1d5fcc16524f9dde
lokii101 <vasy_1605@mail.ru>
2025-11-18 12:18:53 +0300

    Update main.py

--- 2.7lab/main.py
+++ 2.7lab/main.py
@@ -1,2 +1,14 @@
-if __name__ == "__main__":
-    pass
+def remove_line_from_file(filename, k):
+    try:
+        with open(filename, 'r', encoding='utf-8') as file:
+            lines = file.readlines()
+        if 1 <= k <= len(lines):
+            lines.pop(k - 1)
+            with open(filename, 'w', encoding='utf-8') as file:
+                file.writelines(lines)
+    except FileNotFoundError:
+        print({filename})
+    except PermissionError:
+        print({filename})
+    except Exception as e:
+        print({e})
